1cb1ca625aaba3dc03a5fcffcd52430553df26bf
khalid harbi <ka901od@gmail.com>
2023-11-19 22:45:54 +0300

    init

--- dateNum.js
+++ dateNum.js
@@ -2,13 +2,20 @@ const d = new Date(2020, 9, 17)
 //console.log(d.getDay());
 //console.log(d.getFullYear());
 //console.log(d.getMonth());
-console.log(d.getDate());
+//console.log(d.getDate());
 
 const number = Math.ceil(-9.12);
 const num = Math.floor(-9.12);
 const N = Math.round(9.52);
 const n = Math.abs(-9);
-console.log(n);
-console.log(N);
-console.log(num);
-console.log(number);
+//console.log(n);
+//console.log(N);
+//console.log(num);
+//console.log(number);
+
+const numbers = [4, 2, -9, 9, 7];
+
+const snum = Math.min(...numbers);
+console.log(snum);
+const mnum = Math.max(...numbers);
+console.log(mnum);
